e3bc7c75ff06de0be6ce53d31207fc302904a9c0
sam20799 <shubham.2k07@gmail.com>
2026-01-15 03:10:59 +0530

    updated test_db_helper file, did unit testing with updated data

--- tests/backend/test_db_helper.py
+++ tests/backend/test_db_helper.py
@@ -3,9 +3,9 @@ from backend import db_helper
 def test_expense_for_date():
     expenses = db_helper.fetch_expense_for_date('2025-08-15')
     assert len(expenses) == 1
-    assert expenses[0]['amount'] == 10
-    assert expenses[0]['category'] == 'Shopping'
-    assert expenses[0]['notes'] == 'Bought potatoes'
+    assert expenses[0]['amount'] == 45
+    assert expenses[0]['category'] == 'Food'
+    assert expenses[0]['notes'] == 'Vegetables'
 
 def test_expenses_invalid_date():
     expenses = db_helper.fetch_expense_for_date('9999-08-15')
