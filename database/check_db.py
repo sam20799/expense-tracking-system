388d46758a0fee414da61b20ade82e31d3ab961d
sam20799 <shubham.2k07@gmail.com>
2026-01-14 23:03:42 +0530

    created and tested postgresql database

--- database/check_db.py
+++ database/check_db.py
@@ -1,13 +1,25 @@
-import sqlite3
-import pandas as pd
-from pathlib import Path
+import psycopg2
+from psycopg2.extras import RealDictCursor  # optional, to get results as dicts
+import os
 
-db_path = Path(__file__).parent / "expenses.db"
-conn = sqlite3.connect(db_path)
+DATABASE_URL = "postgresql://expenses_db_a5u5_user:W81hb8nLTx7PP884sobVCoC9nhaedMtK@dpg-d5jr9h7gi27c73e1mkfg-a.singapore-postgres.render.com/expenses_db_a5u5"
 
-df = pd.read_sql_query("SELECT * FROM expenses", conn)
+# Connect to DB
+conn = psycopg2.connect(DATABASE_URL)
+cursor = conn.cursor(cursor_factory=RealDictCursor)  # optional: get dict instead of tuple
 
+# Execute SELECT query
+cursor.execute("SELECT * FROM expenses")
+
+# Fetch all rows
+rows = cursor.fetchall()
+
+# Print rows
+for row in rows:
+    print(row)
+
+# Close connection
+cursor.close()
 conn.close()
 
-# put a breakpoint on print and run in debug mode and see ad df : better view
-print(df)
+
